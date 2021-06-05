7ada3a32108844adbca89601ff57d3fe97e1dd1d
Usman Ahmed Gondal <u.ag@icloud.com>
2021-06-05 13:33:56 +0500

    adding *gem to .gitignore

---
+++ lib/octopush.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "octopush/version"
+
+module Octopush
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/octopush_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Octopush do
+  it "has a version number" do
+    expect(Octopush::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
